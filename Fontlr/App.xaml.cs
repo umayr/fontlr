6db0bde83746a99e61e34621513317d8de2df561
Umayr Shahid <umayrr@hotmail.co.uk>
2014-12-18 00:00:01 +0500

    application configurations

---
+++ Fontlr/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Elysium;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Fontlr
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void StartupHandler(object sender, System.Windows.StartupEventArgs e)
+        {
+            //this.Apply(Theme.Dark, AccentBrushes.Blue, Brushes.White);
+            
+        }
+
+    }
+}
+
